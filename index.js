1e21e31f74fac113caa6516fbbd8cd20a51a7bb8
Li Keny <74258644+keni2002@users.noreply.github.com>
2023-12-18 21:34:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let boton =  document.getElementById("boton");
 
+boton.addEventListener("onclick", function() {
+  let inputValue = document.getElementById("input").value;
+  alert(inputValue);
+});
